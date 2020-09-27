db5e93bcdc9c4529e2572b98f231ec61b70b340d
REDskyTM <petuhok666@gmail.com>
2020-09-27 13:53:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 settings ={
-    'token': 'NzU5MDY1MDY2NjgyNTgwOTkz.X24Ebg.oDzzZRGFNd6uJUaCaZDDY7YJznM',
+    'token': 'NzU5MDY1MDY2NjgyNTgwOTkz.X24Ebg.wEBuckgZ1TnbUny7j-q1q-9kVGk',
     'bot': 'OLivka',
     'client_id': '759065066682580993'}
